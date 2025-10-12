4ca79246c668108d6b9d7ad7e6ad8dc9ceaf58c8
Yuvraj-508 <yuvi97437@gmail.com>
2025-10-12 23:15:41 +0530

    build

--- backend/Routes/User.js
+++ backend/Routes/User.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userRouter = express.Router();
-const User = require('../models/user');
+const User = require('../Models/user');
 const  {addUser, getUserList, getUserById } = require('../Controllers/User');
 
 
