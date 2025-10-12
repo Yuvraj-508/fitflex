46981a7d5b29202c2a7bca440fcc410c7e4b20fe
Yuvraj-508 <yuvi97437@gmail.com>
2025-10-13 00:49:38 +0530

    h

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
-import userRouter from "./routes/User.js";
+import userRouter from "./routes/user.js";
 
 const server = express();
 const PORT = process.env.PORT || 3000;
