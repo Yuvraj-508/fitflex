cfa3ce5433b683f51b97a3b5c09e70d523a0e37d
Yuvraj-508 <yuvi97437@gmail.com>
2025-10-12 23:50:12 +0530

    cc

--- backend/Controllers/User.js
+++ backend/Controllers/User.js
@@ -1,4 +1,4 @@
-const User = require('../Models/User.js');
+const User = require('../Models/User');
 // const fs = require('fs');
 // const path = require('path');
 // const PDFDocument = require('pdfkit');
