a1c779346b6c1250abbe4588a5a4bbbf46d13802
Yuvraj-508 <yuvi97437@gmail.com>
2025-10-13 00:23:28 +0530

    ff

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const userRouter = require('./Routes/User.js');
 const server= express();
+import 'dotenv/config';
+import connectDB from './config/db.js';
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 
+await connectDB();
 const corsOptions = {
     origin:[ 'http://localhost:5173','https://fitflex-chi.vercel.app'],
      credentials: true
