eb28e3f1cb0daf4830feab9151a066770c3e4a66
Yuvraj-508 <yuvi97437@gmail.com>
2025-10-12 22:56:47 +0530

    bugg2

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 
 const corsOptions = {
     origin:[ 'http://localhost:5173','https://fitflex-chi.vercel.app'],
+     credentials: true
 };
 server.use(cors(corsOptions));
 server.use(express.json());
